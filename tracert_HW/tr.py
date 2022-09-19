26f38feab94779b2b77dbae4a0e79e654ee5b67a
winnower-HUAWEI <1427035192@qq.com>
2022-09-19 17:41:07 +0800

    优化算法

--- tracert_HW/tr.py
+++ tracert_HW/tr.py
@@ -16,9 +16,12 @@ G = nx.Graph()
 def tra(name: str):
     # dstIP = '58.192.118.142'
     dstIP = socket.gethostbyname(name)
+    print(f"\ntracerouting\t{name}:")
+
+    link = []
 
     taps = 20
-    lastNode = set(["host"])
+    # lastNode = set()
 
     a = IP(dst=dstIP) / ICMP()
     for tap in range(taps):
@@ -32,12 +35,13 @@ def tra(name: str):
                     node.add(pkt[IP].src)
                 if (pkt[ICMP].type == 0):
                     print(f"Reach!!! {dstIP}")
-                    node.add(name)
-                    for n in node:
-                        G.add_node(n)
-                        for ltN in lastNode:
-                            G.add_edge(ltN, n)
-                    return
+                    # node.add(name)
+                    # for n in node:
+                    #     G.add_node(n)
+                    #     for ltN in lastNode:
+                    #         G.add_edge(ltN, n)
+                    link += [name]
+                    return link
 
         if (len(node) > 0):
             print(node)
@@ -46,17 +50,30 @@ def tra(name: str):
             node.add('*')
             if (tap == taps - 1):
                 print("Unreachable!!!")
-                return
+                return []
 
-        for n in node:
-            G.add_node(n)
-            for ltN in lastNode:
-                G.add_edge(ltN, n)
-        lastNode = node
+        link += [node.pop()]
 
-hostnames=["www.seu.edu.cn","ehall.seu.edu.cn","jwc.seu.edu.cn","newxk.urp.seu.edu.cn","seu.olab.top","pub.seu.edu.cn","dekt.seu.edu.cn","lib.seu.edu.cn","tj.seu.edu.cn","phylab.seu.edu.cn","cyber.seu.edu.cn","nic.seu.edu.cn","gsas.seu.edu.cn"]
+        # for n in node:
+        #     G.add_node(n)
+        #     for ltN in lastNode:
+        #         G.add_edge(ltN, n)
+        # lastNode = node
+
+
+hostnames = [
+    "www.seu.edu.cn", "ehall.seu.edu.cn", "jwc.seu.edu.cn",
+    "newxk.urp.seu.edu.cn", "seu.olab.top", "pub.seu.edu.cn",
+    "dekt.seu.edu.cn", "lib.seu.edu.cn", "tj.seu.edu.cn", "phylab.seu.edu.cn",
+    "cyber.seu.edu.cn", "nic.seu.edu.cn", "gsas.seu.edu.cn"
+]
 for hostname in hostnames:
-    tra(hostname)
+    link = tra(hostname)
+    last = "host"
+    G.add_node(last)
+    for i in link:
+        G.add_edge(last, i)
+        last = i
 
 nx.draw(
     G,
