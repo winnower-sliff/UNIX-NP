85dcbb12caffeae513a32b64a7a825270341dfc9
winnower-HUAWEI <1427035192@qq.com>
2022-09-21 10:23:16 +0800

    修改变量名

--- tracert_HW/tr.py
+++ tracert_HW/tr.py
@@ -13,21 +13,21 @@ G = nx.Graph()
 #     G.add_node(node)
 
 
-def tra(name: str):
+def myTracert(name: str):
     # dstIP = '58.192.118.142'
     dstIP = socket.gethostbyname(name)
     print(f"\ntracerouting\t{name}:")
 
-    link = []
+    nodeLink = []
 
     taps = 20
     # lastNode = set()
 
-    a = IP(dst=dstIP) / ICMP()
+    icmpPkt = IP(dst=dstIP) / ICMP()
     for tap in range(taps):
-        a[IP].ttl = tap
+        icmpPkt[IP].ttl = tap
         print(f"TTL: {tap}\t", end="")
-        ans, unans = sr([a] * 3, timeout=3, verbose=0)  # verbose表示sr()是否展示详细信息
+        ans, unans = sr([icmpPkt] * 3, timeout=3, verbose=0)  # verbose表示sr()是否展示详细信息
         node = set()
         for pkts_snd_rcv in ans:
             for pkt in pkts_snd_rcv:
@@ -40,12 +40,12 @@ def tra(name: str):
                     #     G.add_node(n)
                     #     for ltN in lastNode:
                     #         G.add_edge(ltN, n)
-                    link += [name]
-                    return link
+                    nodeLink += [name]
+                    return nodeLink
 
         if (len(node) > 0):
             print(node)
-            link += [node.pop()]
+            nodeLink += [node.pop()]
 
         else:
             print('*')
@@ -69,12 +69,12 @@ hostnames = [
     "cyber.seu.edu.cn", "nic.seu.edu.cn", "gsas.seu.edu.cn"
 ]
 for hostname in hostnames:
-    link = tra(hostname)
-    last = "host"
-    G.add_node(last)
-    for i in link:
-        G.add_edge(last, i)
-        last = i
+    nodeLink = myTracert(hostname)
+    lastNode = "host"
+    G.add_node(lastNode)
+    for nextNode in nodeLink:
+        G.add_edge(lastNode, nextNode)
+        lastNode = nextNode
 
 nx.draw(
     G,
