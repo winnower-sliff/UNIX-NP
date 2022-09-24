61101b45a7a035c27a486606ab52718dfd5a3c3f
winnower-HUAWEI <1427035192@qq.com>
2022-09-25 00:40:32 +0800

    graph文件存储读取&合并

--- tracert_HW/tr.py
+++ tracert_HW/tr.py
@@ -13,12 +13,12 @@ G = nx.Graph()
 #     G.add_node(node)
 
 
-def myTracert(name: str):
+def myTracert(name: str, whoami: str):
     # dstIP = '58.192.118.142'
     dstIP = socket.gethostbyname(name)
     print(f"\ntracerouting\t{name}:")
 
-    nodeLink = []
+    nodeLink = [whoami]
 
     taps = 20
     # lastNode = set()
@@ -27,7 +27,8 @@ def myTracert(name: str):
     for tap in range(taps):
         icmpPkt[IP].ttl = tap
         print(f"TTL: {tap}\t", end="")
-        ans, unans = sr([icmpPkt] * 3, timeout=3, verbose=0)  # verbose表示sr()是否展示详细信息
+        ans, unans = sr([icmpPkt] * 3, timeout=3,
+                        verbose=0)  # verbose表示sr()是否展示详细信息
         node = set()
         for pkts_snd_rcv in ans:
             for pkt in pkts_snd_rcv:
@@ -40,7 +41,7 @@ def myTracert(name: str):
                     #     G.add_node(n)
                     #     for ltN in lastNode:
                     #         G.add_edge(ltN, n)
-                    nodeLink += [name]
+                    nodeLink += [name + "\n" + pkt[IP].src]
                     return nodeLink
 
         if (len(node) > 0):
@@ -50,11 +51,11 @@ def myTracert(name: str):
         else:
             print('*')
             # node.add('*')
+            nodeLink += ['*']
             if (tap == taps - 1):
                 print("Unreachable!!!")
-                return []
+                return nodeLink + [name + "\nunreachable"]
 
-        
         # for n in node:
         #     G.add_node(n)
         #     for ltN in lastNode:
@@ -62,23 +63,45 @@ def myTracert(name: str):
         # lastNode = node
 
 
-hostnames = [
-    "www.seu.edu.cn", "ehall.seu.edu.cn", "jwc.seu.edu.cn",
-    "newxk.urp.seu.edu.cn", "seu.olab.top", "pub.seu.edu.cn",
-    "dekt.seu.edu.cn", "lib.seu.edu.cn", "tj.seu.edu.cn", "phylab.seu.edu.cn",
-    "cyber.seu.edu.cn", "nic.seu.edu.cn", "gsas.seu.edu.cn"
-]
-for hostname in hostnames:
-    nodeLink = myTracert(hostname)
-    lastNode = "host"
-    G.add_node(lastNode)
-    for nextNode in nodeLink:
-        G.add_edge(lastNode, nextNode)
-        lastNode = nextNode
+def main(hostnames, filename, whoami):
+    for hostname in hostnames:
+        nodeLink = myTracert(hostname, whoami=whoami)
+        if (len(nodeLink) == 0):
+            continue
+        lastNode = nodeLink[0]
+        G.add_node(lastNode)
+        starNum = 0
+        for nextNodeNo in range(1, len(nodeLink)):
+            if (nodeLink[nextNodeNo] == '*'):
+                starNum += 1
+                continue
+            G.add_edge(lastNode, nodeLink[nextNodeNo], score='*' * starNum)
+            starNum = 0
+            lastNode = nodeLink[nextNodeNo]
 
-nx.draw(
-    G,
-    with_labels=True,
-    node_color='y',
-)
-plt.show()
+    nx.write_gpickle(G, filename)
+
+
+hostnames_inside = [
+    "www.seu.edu.cn",
+    "ehall.seu.edu.cn",
+    "jwc.seu.edu.cn",
+    "newxk.urp.seu.edu.cn",
+    "pub.seu.edu.cn",
+    "dekt.seu.edu.cn",
+    "lib.seu.edu.cn",
+    "tj.seu.edu.cn",
+    "phylab.seu.edu.cn",
+    "cyber.seu.edu.cn",
+    "nic.seu.edu.cn",
+    "gsas.seu.edu.cn",
+]
+hostnames_outside = [
+    "www.seu.edu.cn", "ehall.seu.edu.cn", "jwc.seu.edu.cn", "www.bilibili.com",
+    "www.baidu.com", "8.8.8.8"
+]
+# hostnames = ["www.seu.edu.cn","ehall.seu.edu.cn", "jwc.seu.edu.cn",]
+whoami = 'm4'
+for times in range(3):
+    main(hostnames_inside, whoami + f'_inside{times}.gpickle', whoami)
+    main(hostnames_outside, whoami + f'_outside{times}.gpickle', whoami)
