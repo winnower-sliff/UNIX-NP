3287626fc9f9cf28a2440b65f6e05661234cc71c
winnower-Inspiron5488 <1427035192@qq.com>
2022-09-25 15:04:56 +0800

    测试用

---
+++ tracert_HW/joinAll.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# import multiprocessing
+# from tabnanny import verbose
+# from time import sleep
+from scapy.all import *
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G_list = []
+whoami='suzhou'
+for times in range(3):
+    A = nx.read_gpickle(f"{whoami}_inside{times}.gpickle")
+    B = nx.read_gpickle(f"{whoami}_outside{times}.gpickle")
+    G_list += [nx.compose(A, B)]
+G = nx.compose_all(G_list)
+pos = nx.spring_layout(G, scale=10)
+
+nx.draw(
+    G,
+    pos=pos,
+    with_labels=True,
+    node_color='y',
+    font_size=6,
+    # node_size=800
+)
+edge_labels = nx.get_edge_attributes(G, 'score')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+# nx.draw_networkx_labels(G,pos,alpha=0.5)
+
+plt.show()
